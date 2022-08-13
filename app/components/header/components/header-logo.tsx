ff9ae9a04676c948df84bf6a14f4214587ed5f19
sunsky <hikerherd@serant.fr>
2022-08-13 15:39:27 +0200

    logo

--- app/components/header/components/header-logo.tsx
+++ app/components/header/components/header-logo.tsx
@@ -10,9 +10,9 @@ const HeaderLogo: FC = () => {
   return (
     <Link href={Routes.HomePage()} passHref>
       <HStack as="a">
-        <LogoIcon w={5} h={5} />
+        <LogoIcon w={8} h={8} />
         <Heading size="md" display={{ base: "none", md: "block" }}>
-          hikerherd
+          pack.serant.fr
         </Heading>
       </HStack>
     </Link>
